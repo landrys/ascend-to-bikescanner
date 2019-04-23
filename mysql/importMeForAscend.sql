96e514b3d0f990331a5c8c7611680bdea6f20a2f
landrys <landryseleven@gmail.com>
2019-04-23 10:25:45 -0400

    Fixed table name in import script

--- mysql/importMeForAscend.sql
+++ mysql/importMeForAscend.sql
@@ -1,5 +1,5 @@
 source ~/ascendToBikescanner/mysql/ascendSerialized.sql
 source ~/ascendToBikescanner/mysql/ascendToBikescanner.sql
 source ~/ascendToBikescanner/mysql/ilselevenac.sql
-source ~/ascendToBikescanner/mysql/latestscanelevenac.sql
+source ~/ascendToBikescanner/mysql/latestScanElevenAc.sql
 source ~/ascendToBikescanner/mysql/product.sql
